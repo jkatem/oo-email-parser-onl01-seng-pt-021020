c0bf23f590d7301969d223ce651e59a95306a5c8
Jen Miao <jenmiao@gmail.com>
2020-03-05 07:00:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,13 +4,16 @@
 # or whitespace (' ').
 
 class EmailParser
-  attr_accessor :emails
 
-  def initialize(email)
-    @emails = email
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
 
   def parse
-    emails.delete(',').split.uniq
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
-end 
+
+end
